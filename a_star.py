85e92538427a3e5b0cbfe6813cb66a7cbda78947
dhanush babu allam <dhanu0412@gmail.com>
2023-03-20 22:20:44 -0400

    done visualization

--- a_star.py
+++ a_star.py
@@ -1,71 +1,215 @@
 import numpy as np
 from queue import PriorityQueue as pq
-import pygame
+import pygame as pg
 import math
+import matplotlib.pyplot as plt
+from sortedcollections import OrderedSet
 
-obstacle_space=[]
+obstacle_space=OrderedSet()
 table=[]
 goal_threshold=[]
 open_list=pq()
 c2c=[]
+occupied_space_x=[]
+occupied_space_y=[]
 check_reach=False
 
-def map(cleareance):
+def change_points(points, length):
+    return (points[0], length - points[1])
+
+def change_points_rect(points, length, obj_height):
+    return (points[0], length - points[1] - obj_height)
+
+def intersect_detect(slope_1, slope_2, const_1, const_2, coeff_1, coeff_2):
+
+    first = np.array([[-slope_1, coeff_1], [-slope_2, coeff_2]])
+    second = np.array([const_1, const_2])
+    combined = np.linalg.solve(first, second)
+    return combined
+
+def parallel_line_finder(slope, const, bloat, str):
+    first = const + bloat*np.sqrt(slope**2 + 1)
+    second = const - bloat*np.sqrt(slope**2 + 1)
+    if str == 'upper':
+
+        if first > const:
+            return first
+        else:
+
+            return second
+    else:
+
+        if first < const:
+
+            return first
+        else:
+            return second
+
+def clearance(bloat, checker):
+
+    hexagon_coeff_1 = hexagon_coeff_3 = -15/26
+    hexagon_coeff_2 = hexagon_coeff_4 =  15/26
+
+    hexagon_const_1, hexagon_const_2, hexagon_const_3  = 4850/13, 350/13, 2900/13
+    hexagon_const_4, hexagon_const_5, hexagon_const_6 = -1600/13, 235, 365
+
+    triangle_coeff_1 = -2
+    triangle_coeff_2 =  2
+
+    triangle_const_1 = 1145
+    triangle_const_2 = -895
+    triangle_const_3 = 460
+
+    parallel_seg_hex_1 = parallel_line_finder(hexagon_coeff_1, hexagon_const_1, bloat, "upper")
+    parallel_seg_hex_2 = parallel_line_finder(hexagon_coeff_2, hexagon_const_2, bloat, "upper")
+    parallel_seg_hex_3 = parallel_line_finder(hexagon_coeff_3, hexagon_const_3, bloat, "lower")
+    parallel_seg_hex_4 = parallel_line_finder(hexagon_coeff_4, hexagon_const_4, bloat, "lower")
+
+    parallel_seg_tri_1 = parallel_line_finder(triangle_coeff_1, triangle_const_1, bloat, "upper")
+    parallel_seg_tri_2 = parallel_line_finder(triangle_coeff_2, triangle_const_2, bloat, "lower")
+    if checker == 1:
+        return parallel_seg_hex_1, parallel_seg_hex_2, parallel_seg_hex_3, parallel_seg_hex_4, parallel_seg_tri_1, parallel_seg_tri_2
 
 
-    for x in range(0,601):
-        for y in range(0,cleareance+1):
-            obstacle_space.append((x,y))
+    intersect_hex_1 = intersect_detect(hexagon_coeff_1, hexagon_coeff_2, parallel_seg_hex_1, parallel_seg_hex_2, 1, 1)
 
-    for x in range(0,601):
-        for y in range(251-cleareance-1,251):
-            obstacle_space.append((x,y))
+    intersect_hex_2 = intersect_detect(hexagon_coeff_1, -1, parallel_seg_hex_1, hexagon_const_6+bloat, 1, 0)
+    intersect_hex_3 = intersect_detect(hexagon_coeff_4, -1, parallel_seg_hex_4, hexagon_const_6+bloat, 1, 0)
 
-    for x in range(0,cleareance+1):
-        for y in range(0,251):
-            obstacle_space.append((x,y))
+    intersect_hex_4 = intersect_detect(hexagon_coeff_3, hexagon_coeff_4, parallel_seg_hex_3, parallel_seg_hex_4, 1, 1)
 
-    for x in range(601-cleareance,601):
-        for y in range(0,251):
-            obstacle_space.append((x,y))
+    intersect_hex_5 = intersect_detect(hexagon_coeff_3, -1, parallel_seg_hex_3, hexagon_const_5-bloat, 1, 0)
+    intersect_hex_6 = intersect_detect(hexagon_coeff_2, -1, parallel_seg_hex_2, hexagon_const_5-bloat, 1, 0)
+
+    intersect_tri_1 = intersect_detect(triangle_coeff_1, triangle_coeff_2, parallel_seg_tri_1, parallel_seg_tri_2, 1, 1)
+    intersect_tri_2 = intersect_detect(triangle_coeff_1, 0, parallel_seg_tri_1, 225 + bloat, 1, 1)
+
+    intersect_tri_3 = [triangle_const_3 - bloat, 225 + bloat]
+    intersect_tri_4 = [triangle_const_3 - bloat, 25 - bloat]
     
-    for x in range(100 - cleareance, 150 + cleareance + 1):
-        for y in range(0, 250+1):
-            if y <= (100 + cleareance) or y >= (150 - cleareance):
-                obstacle_space.append((x,y))
-    
-    for x in range(460-cleareance, 510+2*cleareance):
-        for y in range(0, 250):
-            m_1, m_2 = 2, 2
-            b_1, b_2 = 895, 1145
-            if (m_1*x - y <= b_1) and (m_2*x+y <= b_2):
-                    obstacle_space.append((x,y))
-            
-            c_1 = b_1 + cleareance * (math.sqrt(pow(m_2,2) + 1))
-            c_2 = b_1 - cleareance * (math.sqrt(pow(m_2,2) + 1))
-            c_3 = b_2 + cleareance * (math.sqrt(pow(m_1,2) + 1))
-            c_4 = b_2 - cleareance * (math.sqrt(pow(m_1,2) + 1))
+    intersect_tri_5 = intersect_detect(triangle_coeff_2, 0, parallel_seg_tri_2, 25 - bloat, 1, 1)
 
-            if (m_1*x-y <= min(c_1, c_2)) and (m_2*x+y <= min(c_3, c_4)):
-                    obstacle_space.append((x,y))
-    for x in range(300 - int(64.95) - cleareance, 300 + int(64.95) + cleareance):
-        for y in range(125 - 75 - cleareance, 125 + 75 + cleareance):
-            m = 0.577
-            b_1, b_2, b_3, b_4 = 32.692, 378.846, 217.307, 128.846
-            if  (y - m*x - b_1) < 0 and (y + m*x - b_2) < 0 and (y + m*x - b_3) > 0 and (y - m*x + b_4) > 0:
-                obstacle_space.append((x,y))
-            
-            c_1 = b_1 + cleareance * (math.sqrt(pow(m,2) + 1))
-            c_2 = b_1 - cleareance * (math.sqrt(pow(m,2) + 1))
-            c_3 = b_2 + cleareance * (math.sqrt(pow(m,2) + 1))
-            c_4 = b_2 - cleareance * (math.sqrt(pow(m,2) + 1))
-            c_5 = b_3 + cleareance * (math.sqrt(pow(m,2) + 1))
-            c_6 = b_3 - cleareance * (math.sqrt(pow(m,2) + 1))
-            c_7 = b_4 + cleareance * (math.sqrt(pow(m,2) + 1))
-            c_8 = b_4 - cleareance * (math.sqrt(pow(m,2) + 1))
+    return intersect_hex_1, intersect_hex_2, intersect_hex_3, intersect_hex_4, intersect_hex_5, intersect_hex_6, intersect_tri_1, intersect_tri_2, intersect_tri_3, intersect_tri_4, intersect_tri_5
+      
 
-            if  (y - m*x - min(c_1, c_2)) < 0 and (y + m*x - min(c_3, c_4)) < 0 and (y+ m*x - min(c_5, c_6)) > 0 and (y - m*x + min(c_7, c_8)) > 0:
-                obstacle_space.append((x,y))
+def Branching_lines(display, col_1, col_3, begin, end, rad_conv):
+
+    pg.draw.line(display, col_1, begin, end, 1)
+
+    orientation = math.degrees(math.atan2(begin[1] - end[1], end[0] - begin[0])) + 90
+
+    pg.draw.polygon(display, col_3, ((end[0]+rad_conv*math.sin(math.radians(orientation)), end[1]+rad_conv*math.cos(math.radians(orientation))),
+                                     
+                                           (end[0]+rad_conv*math.sin(math.radians(orientation-120)),
+                                            end[1]+rad_conv*math.cos(math.radians(orientation-120))),
+
+                                           (end[0]+rad_conv*math.sin(math.radians(orientation+120)), end[1]+rad_conv*math.cos(math.radians(orientation+120)))))
+
+
+
+
+def game(bloat, hex_c1, hex_c2, hex_c3, hex_c4, hex_c5, hex_c6, tri_c1, tri_c2, tri_c3, tri_c4, tri_c5, visited, optimal_path, path):
+    pg.init()
+    size = [600, 250]
+    display_view = pg.display.set_mode(size)
+    pg.display.set_caption("Visualization")
+    #video = vidmaker.Video("Explore.mp4", late_export=True)
+    clock = pg.time.Clock()
+    running = True
+
+    rect_l_clr_1, rect_l_clr_11  = change_points_rect([100-bloat, 0], 250, 100+bloat)
+    rect_l_wclr_2, rect_l_wclr_22 = change_points_rect([100, 0], 250, 100)
+    rect_u_clr_3, rect_u_clr_33 = change_points_rect([100-bloat, 150-bloat], 250, 100+bloat)
+    rect_u_clr_4, rect_u_clr_44 = change_points_rect([100, 150], 250, 100)
+
+    tri_pt_1, tri_pt_11 = change_points(tri_c1, 250)
+    tri_pt_2, tri_pt_22 = change_points(tri_c2, 250)
+    tri_pt_3, tri_pt_33 = change_points(tri_c3, 250)
+    tri_pt_4, tri_pt_44 = change_points(tri_c4, 250)
+    tri_pt_5, tri_pt_55 = change_points(tri_c5, 250)
+
+    tri_const_1, tri_const_11 = change_points([460, 25], 250)
+    tri_const_2, tri_const_22 = change_points([460, 225], 250)
+    tri_const_3, tri_const_33 = change_points([510, 125], 250)
+
+    hex_const_1, hex_const_11 = change_points(hex_c1, 250)
+    hex_const_2, hex_const_22 = change_points(hex_c2, 250)
+    hex_const_3, hex_const_33 = change_points(hex_c3, 250)
+
+    hex_const_4, hex_const_44 = change_points(hex_c4, 250)
+    hex_const_5, hex_const_55 = change_points(hex_c5, 250)
+    hex_const_6, hex_const_66 = change_points(hex_c6, 250)
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        pg.draw.rect(display_view, "blue", [rect_l_clr_1, rect_l_clr_11, 50+2*bloat, 100+bloat], 0)
+        pg.draw.rect(display_view, "red", [rect_l_wclr_2, rect_l_wclr_22, 50, 100], 0)
+        pg.draw.rect(display_view, "blue", [rect_u_clr_3, rect_u_clr_33, 50+2*bloat, 100+bloat], 0)
+        pg.draw.rect(display_view, "red", [rect_u_clr_4, rect_u_clr_44, 50, 100], 0)
+        pg.draw.rect(display_view, "blue", [0, 0, bloat, 250], 0)
+        pg.draw.rect(display_view, "blue", [0, 0, 600, bloat], 0)
+        pg.draw.rect(display_view, "blue", [0, 250-bloat, 600, bloat], 0)
+        pg.draw.rect(display_view, "blue", [600-bloat, 0, bloat, 250], 0)
+        pg.draw.polygon(display_view, "blue", ([tri_pt_1, tri_pt_11], [tri_pt_2, tri_pt_22], [
+                            tri_pt_3, tri_pt_33], [tri_pt_4, tri_pt_44], [tri_pt_5, tri_pt_55]), 0)
+        pg.draw.polygon(
+            display_view, "red", [[tri_const_1, tri_const_11], [tri_const_2, tri_const_22], [tri_const_3, tri_const_33]], 0)
+        pg.draw.polygon(display_view, "blue", [[hex_const_1, hex_const_11], [hex_const_2, hex_const_22], [
+                            hex_const_3, hex_const_33], [hex_const_4, hex_const_44], [hex_const_5, hex_const_55], [hex_const_6, hex_const_66]], 0)
+        pg.draw.polygon(display_view, "red", ((
+            235, 87.5), (300, 50), (365, 87.5), (365, 162.5), (300, 200), (235, 162.5)))
+        for l in range(len(visited)-2):
+            transition = visited[l]
+            for i in range (len (path)):
+                if path[i][2]==transition:
+                    mat = path[i][1]
+            transition = change_points(transition, 250)
+            mat = change_points(mat, 250)
+            #video.update(pg.surfarray.pixels3d(
+            #display_view).swapaxes(0, 1), inverted=False)
+            Branching_lines(display_view, "white", (127, 255, 212),
+                  [transition[0], transition[1]], [mat[0], mat[1]], 0.5)
+            pg.display.flip()
+            clock.tick(300)
+
+        for i in optimal_path:
+            pg.draw.circle(display_view, (0, 255, 255), change_points(i, 250), radius-3)
+            #video.update(pg.surfarray.pixels3d(
+            #display_view).swapaxes(0, 1), inverted=False)
+            pg.display.flip()
+            clock.tick(20)
+        running = False
+    pg.display.flip()
+    pg.time.wait(3000)
+    pg.quit()
+
+def orientation_block(bloat, hex_c1, hex_c2, hex_c3, hex_c4, tri_c1, tri_c2):
+    occupied_space = OrderedSet()
+    for x in np.arange(0, 601, 0.5):
+        for y in np.arange(0, 251, 0.5):
+            if (100 - bloat <=x<=150 + bloat) and (0<=y<=105):
+                occupied_space.add((x, y))
+
+            if (105 - bloat<=x<=145 + bloat) and (0<=y<=100):
+                occupied_space.add((x, y))
+
+            if (100 - bloat<=x<=150 + bloat) and (150 - bloat<=y<=245 + bloat):
+                occupied_space.add((x, y))
+
+            if (105 - bloat<=x<= 145 + bloat) and (155 - bloat<=y<=245 + bloat):
+                occupied_space.add((x, y))    
+
+            if (y+(2*x)-tri_c1) <= 0 and (y-(2*x)-tri_c2) >= 0 and ((460 - bloat)<=x) and ((25 - bloat)<= y and y <= (225 + bloat)):
+                occupied_space.add((x, y))
+
+            if (y-(15/26)*x - hex_c2) <= 0 and (y+(15/26)*x - hex_c1) <= 0 and (y-(15/26)*x - hex_c4) >= 0 and (y+(15/26)*x-hex_c3) >= 0 and ((235-bloat)<=x<=(365+bloat)):
+                occupied_space.add((x, y))
+                
+            if (x <= 0 + bloat) or (y <= 0 + bloat) or (x >= 600 - bloat) or (y >= 250 - bloat):
+                occupied_space.add((x, y))
+    return occupied_space
 
 def roundoff(x):
     x_floor=math.floor(x)
@@ -121,7 +265,7 @@ def zero_action(node,visited,stepsize,c2c,goal):
                                     return
                                 else:
                                     return
-            table.append([cost,node,new_node])
+            table.append([cost,node,new_node,0])
             open_list.put((cost,new_c2g,new_c2c,new_node))
             if dist(new_node,goal)<1.5:
                 check_reach=True
@@ -152,7 +296,7 @@ def minus60_action(node,visited,stepsize,c2c,goal):
                                     return
                                 else:
                                     return
-            table.append([cost,node,new_node])
+            table.append([cost,node,new_node,-2])
             open_list.put((cost,new_c2g,new_c2c,new_node))
             if dist(new_node,goal)<1.5:
                 check_reach=True
@@ -160,7 +304,7 @@ def minus60_action(node,visited,stepsize,c2c,goal):
 def minus30_action(node,visited,stepsize,c2c,goal):
     global check_reach
     new_x=node[0]+stepsize*math.cos(np.deg2rad(node[2]-30))
-    new_y=node[1]+stepsize*math.sin(np.deg2rad(node[2]))
+    new_y=node[1]+stepsize*math.sin(np.deg2rad(node[2]-30))
     new_theta=check_theta(node[2]-30)
     new_x=roundoff(new_x)
     new_y=roundoff(new_y)
@@ -183,7 +327,7 @@ def minus30_action(node,visited,stepsize,c2c,goal):
                                     return
                                 else:
                                     return
-            table.append([cost,node,new_node])
+            table.append([cost,node,new_node,-1])
             open_list.put((cost,new_c2g,new_c2c,new_node))
             if dist(new_node,goal)<1.5:
                 check_reach=True
@@ -214,7 +358,7 @@ def plus30_action(node,visited,stepsize,c2c,goal):
                                     return
                                 else:
                                     return
-            table.append([cost,node,new_node])
+            table.append([cost,node,new_node,1])
             open_list.put((cost,new_c2g,new_c2c,new_node))
             if dist(new_node,goal)<1.5:
                 check_reach=True
@@ -245,10 +389,11 @@ def plus60_action(node,visited,stepsize,c2c,goal):
                                     return
                                 else:
                                     return
-            table.append([cost,node,new_node])
+            table.append([cost,node,new_node,2])
             open_list.put((cost,new_c2g,new_c2c,new_node))
             if dist(new_node,goal)<1.5:
                 check_reach=True
+
 
 cleareance=int(input("input robot clearance "))
 radius=int(input("input robot radius "))
@@ -256,6 +401,16 @@ while(True):
     stepsize=int(input("input stepsize "))
     if stepsize>=1 and stepsize<=10:
         break
+
+f=open("obstacle.txt","w")
+parallel_seg_hex_1, parallel_seg_hex_2, parallel_seg_hex_3, parallel_seg_hex_4, parallel_seg_tri_1, parallel_seg_tri_2 = clearance((cleareance+radius), 1)
+obstacle_space = orientation_block((cleareance + radius), parallel_seg_hex_1, parallel_seg_hex_2, parallel_seg_hex_3, parallel_seg_hex_4, parallel_seg_tri_1, parallel_seg_tri_2)
+intersect_hex_1, intersect_hex_2, intersect_hex_3, intersect_hex_4, intersect_hex_5, intersect_hex_6, intersect_tri_1, intersect_tri_2, intersect_tri_3, intersect_tri_4, intersect_tri_5 = clearance(cleareance, 0)
+#print(obstacle_space)
+#for i in range(len(obstacle_space)):
+ #   f.write(str(obstacle_space[i]))
+#f.close()
+
 
 while(True):
     start_x=int(input("input start x coordinate"))
@@ -273,26 +428,25 @@ while(True):
         goal=(goal_x,goal_y,goal_theta)
         break
 
-map(radius+cleareance)
 open_list.put((dist(start,goal),dist(start,goal),0,start))
 closed_list=np.zeros((1200,500,12),dtype=int)
-closed_list_vis=[]
+closed_list_vis=OrderedSet()
 new_goal=(0,0,0)
 got_goal=0
 back_track=[]
-table.append([0,start,start])
+table.append([0,start,start,0.001])
 while(open_list.empty()==False):
     current_node=open_list.get()
     now_node=current_node[3]
     if closed_list[int(now_node[0]*2)][int(now_node[1]*2)][int(now_node[2]/30)]!=1:
         closed_list[int(now_node[0]*2)][int(now_node[1]*2)][int(now_node[2]/30)]=1
-        closed_list_vis.append((now_node[0],now_node[1]))
+        closed_list_vis.add((now_node[0],now_node[1],now_node[2]))
         if dist(now_node,goal)>1.5:
             if check_reach==False and now_node[0]>0 and now_node[0]+(stepsize*math.cos(np.deg2rad(now_node[2]+30)))<600 and now_node[1]>0 and now_node[1]+(stepsize*math.sin(np.deg2rad(now_node[2]+30)))<250:
                 plus30_action(now_node,closed_list,stepsize,current_node[2],goal)
             if check_reach==False and now_node[0]>0 and now_node[0]+(stepsize*math.cos(np.deg2rad(now_node[2]+60)))<600 and now_node[1]>0 and now_node[1]+(stepsize*math.sin(np.deg2rad(now_node[2]+60)))<250:
                 plus60_action(now_node,closed_list,stepsize,current_node[2],goal)
-            if check_reach==False and now_node[0]>0 and now_node[0]+stepsize<600 and now_node[1]>0 and now_node[1]+stepsize<250:
+            if check_reach==False and now_node[0]>0 and now_node[0]+(stepsize*math.cos(np.deg2rad(now_node[2]+0)))<600 and now_node[1]>0 and now_node[1]+(stepsize*math.sin(np.deg2rad(now_node[2]+0)))<250:
                 zero_action(now_node,closed_list,stepsize,current_node[2],goal)
             if check_reach==False and now_node[0]>0 and now_node[0]+(stepsize*math.cos(np.deg2rad(now_node[2]-60)))<600 and now_node[1]>0 and now_node[1]+(stepsize*math.sin(np.deg2rad(now_node[2]-60)))<250:
                 minus60_action(now_node,closed_list,stepsize,current_node[2],goal)
@@ -300,9 +454,12 @@ while(open_list.empty()==False):
                 minus30_action(now_node,closed_list,stepsize,current_node[2],goal)
         else:
             got_goal=1
+            print('Goal reached')
             new_goal=(now_node[0],now_node[1],now_node[2])
             break
+print(table)
 if got_goal==1:
+    closed_list_vis.discard(start)
     back_node=new_goal
     back_track=[back_node]
     while(True):
@@ -315,3 +472,6 @@ if got_goal==1:
             break
     back_track.reverse()
     print(back_track)
+    game(cleareance,intersect_hex_1,intersect_hex_2,intersect_hex_3,intersect_hex_4,intersect_hex_5,intersect_hex_6,intersect_tri_1,intersect_tri_2,intersect_tri_3,intersect_tri_4,intersect_tri_5,closed_list_vis,back_track,table)
+else:
+    print('No solution')
